435e232cd09f695abfc4c5f6889ebfe45b5c7639
Bagas Wijayanto <baqazz@gmail.com>
2023-11-11 22:18:28 +0700

    CRUD Department : delete department, search department

--- src/main/java/com/bagas/springrestapi/controller/DepartmentController.java
+++ src/main/java/com/bagas/springrestapi/controller/DepartmentController.java
@@ -1,13 +1,14 @@
 package com.bagas.springrestapi.controller;
 
-import com.bagas.springrestapi.model.DepartmentResponse;
-import com.bagas.springrestapi.model.RegisterDepartmentRequest;
-import com.bagas.springrestapi.model.UpdateDepartmentRequest;
-import com.bagas.springrestapi.model.WebResponse;
+import com.bagas.springrestapi.model.*;
 import com.bagas.springrestapi.service.DepartmentService;
+import com.bagas.springrestapi.service.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -15,6 +16,7 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
 
     @PostMapping(
             path = "/departments",
@@ -29,7 +31,8 @@ public class DepartmentController {
 
     @PutMapping(
             path = "/departments/{deptNo}",
-            consumes = MediaType.APPLICATION_JSON_VALUE
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
 
     )
     private WebResponse<DepartmentResponse> updateDepartment(@PathVariable String deptNo, @RequestBody UpdateDepartmentRequest request){
@@ -38,6 +41,54 @@ public class DepartmentController {
         return WebResponse.<DepartmentResponse>builder()
                 .data(departmentResponse).build();
     }
+
+    @GetMapping(
+            path = "/departments/{deptNo}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+
+    )
+    private WebResponse<DepartmentResponse> getDepartmentByDeptNo(@PathVariable String deptNo){
+        DepartmentResponse departmentResponse = departmentService.getDepartmentByDeptNo(deptNo);
+
+        return WebResponse.<DepartmentResponse>builder()
+                .data(departmentResponse).build();
+    }
+
+    @DeleteMapping(
+            path = "/departments/{deptNo}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+
+    )
+    private WebResponse<String> deleteDepartment(@PathVariable String deptNo){
+        departmentService.deleteDepartment(deptNo);
+
+        return WebResponse.<String>builder()
+                .data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/departments/search",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<DepartmentResponse>> searchDepartment(@RequestParam(value = "keyword",required = false)String keyword,
+                                                                  @RequestParam(value = "size",required = false,defaultValue = "10")Integer size,
+                                                                  @RequestParam(value = "page",required = false,defaultValue = "0")Integer page){
+        Page<DepartmentResponse> departmentResponses = departmentService.searchDepartment(keyword,page,size);
+
+        return WebResponse.<List<DepartmentResponse>>builder()
+                .data(departmentResponses.getContent())
+                .paging(PagingResponse.builder()
+                        .currentPage(departmentResponses.getNumber())
+                        .totalPage(departmentResponses.getTotalPages())
+                        .size((int) departmentResponses.getTotalElements()).build())
+                .build();
+
+
+
+    }
+
+
 
 
 
--- src/main/java/com/bagas/springrestapi/service/DepartmentService.java
+++ src/main/java/com/bagas/springrestapi/service/DepartmentService.java
@@ -2,14 +2,21 @@ package com.bagas.springrestapi.service;
 
 import com.bagas.springrestapi.entity.Department;
 import com.bagas.springrestapi.model.DepartmentResponse;
+import com.bagas.springrestapi.model.EmployeeResponse;
 import com.bagas.springrestapi.model.RegisterDepartmentRequest;
 import com.bagas.springrestapi.model.UpdateDepartmentRequest;
 import com.bagas.springrestapi.repository.DepartmentRepository;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -21,6 +28,8 @@ public class DepartmentService {
     @Autowired
     private ValidationService validationService;
 
+
+    @Transactional
     public void registerDepartment(RegisterDepartmentRequest request){
         validationService.validate(request);
 
@@ -30,6 +39,7 @@ public class DepartmentService {
         departmentRepository.save(department);
     }
 
+    @Transactional
     public DepartmentResponse updateDepartment(String deptNo,UpdateDepartmentRequest request){
         validationService.validate(request);
 
@@ -50,5 +60,52 @@ public class DepartmentService {
         return DepartmentResponse.builder()
                 .deptNo(department.getDeptNo())
                 .deptName(department.getDeptName()).build();
+    }
+
+    @Transactional(readOnly = true)
+    public DepartmentResponse getDepartmentByDeptNo(String deptNo){
+
+        Department department = departmentRepository.findById(deptNo)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Department is not found"));
+
+        return toDepartmentResponse(department);
+
+
+    }
+
+    @Transactional
+    public void deleteDepartment(String deptNo){
+
+        Department department = departmentRepository.findById(deptNo)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Department is not found"));
+
+        departmentRepository.delete(department);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<DepartmentResponse> searchDepartment(String keyword, Integer page, Integer size){
+        Specification<Department> specification = (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (Objects.nonNull(keyword)){
+                predicates.add(criteriaBuilder.or(
+                        criteriaBuilder.like(
+                                criteriaBuilder.lower(root.get("deptNo")),"%"+keyword.toLowerCase()+"%"
+                        ),
+                        criteriaBuilder.like(
+                                criteriaBuilder.lower(root.get("deptName")),"%"+keyword.toLowerCase()+"%"
+                        )
+                ));
+            }
+            return query.where(predicates.toArray(new Predicate[0])).getRestriction();
+        };
+
+        Pageable pageable = PageRequest.of(page,size, Sort.by("deptNo").ascending());
+        Page<Department> departments = departmentRepository.findAll(specification,pageable);
+        List<DepartmentResponse> departmentResponseList = departments.stream()
+                .map(this::toDepartmentResponse).toList();
+
+        return new PageImpl<>(departmentResponseList,pageable,departments.getTotalElements());
+
     }
 }
