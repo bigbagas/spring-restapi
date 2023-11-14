c5d9c8b3adadb6be66bcb4ff2e98ab2669440671
Bagas Wijayanto <baqazz@gmail.com>
2023-11-14 10:18:05 +0700

    salary repo add native query untuk call procedure insert salary

--- src/main/java/com/bagas/springrestapi/repository/SalaryRepository.java
+++ src/main/java/com/bagas/springrestapi/repository/SalaryRepository.java
@@ -2,8 +2,27 @@ package com.bagas.springrestapi.repository;
 
 import com.bagas.springrestapi.entity.Salary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public interface SalaryRepository extends JpaRepository<Salary,Integer> {
+
+    @Query(
+            value = "select * from salaries s where s.emp_no = :empNo",
+            nativeQuery = true
+    )
+    Optional<Salary> salaryByEmpNo(Integer empNo);
+
+
+    @Modifying
+    @Query(
+            value = "call insert_salary(:empNo, :salary,:fromDate, :toDate)",
+            nativeQuery = true
+    )
+    public void insertIntoSalary(Integer empNo, Integer salary, Date fromDate, Date toDate);
 }
