405ddb512123ad2b626568980dd9366c64bc50ef
Bagas Wijayanto <baqazz@gmail.com>
2023-11-15 13:16:27 +0700

    HATEOAS add self link ke controller: SalaryController, TitleController

--- src/main/java/com/bagas/springrestapi/controller/SalaryController.java
+++ src/main/java/com/bagas/springrestapi/controller/SalaryController.java
@@ -6,11 +6,15 @@ import com.bagas.springrestapi.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -26,9 +30,12 @@ public class SalaryController {
     )
     public WebResponse<String> registerSalary(@RequestBody RegisterSalaryRequest request) throws ParseException {
         salaryService.registerSalary(request);
-        return WebResponse.<String>builder()
+        WebResponse<String> webResponse = WebResponse.<String>builder()
                 .data("OK")
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).registerSalary(request)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @GetMapping(
@@ -37,9 +44,13 @@ public class SalaryController {
     )
     public WebResponse<SalaryResponse> getSalaryByEmpNo(@PathVariable("empNo")Integer empNo){
         SalaryResponse salaryResponse = salaryService.getSalaryByEmpNo(empNo);
-        return WebResponse.<SalaryResponse>builder()
+        WebResponse<SalaryResponse> webResponse =WebResponse.<SalaryResponse>builder()
                 .data(salaryResponse)
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).getSalaryByEmpNo(empNo)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
+
     }
 
     @GetMapping(
@@ -49,15 +60,16 @@ public class SalaryController {
     public WebResponse<List<SalaryResponse>> getAllSalary(@RequestParam(value = "page",required = false,defaultValue = "0")Integer page,
                                                           @RequestParam(value = "size",required = false,defaultValue = "10")Integer size){
         Page<SalaryResponse> salaryResponses = salaryService.getAllSalary(page,size);
-
-
-        return WebResponse.<List<SalaryResponse>>builder()
+        WebResponse<List<SalaryResponse>> webResponse = WebResponse.<List<SalaryResponse>>builder()
                 .data(salaryResponses.getContent())
                 .paging(PagingResponse.builder()
                         .currentPage(salaryResponses.getNumber())
                         .totalPage(salaryResponses.getTotalPages())
                         .size((int) salaryResponses.getTotalElements()).build())
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).getAllSalary(page, size)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @PutMapping(
@@ -67,9 +79,12 @@ public class SalaryController {
     public WebResponse<SalaryResponse> updateSalary(@PathVariable("empNo")Integer empNo,
                                                     @RequestBody UpdateSalaryRequest request) throws ParseException {
         SalaryResponse salaryResponse = salaryService.updateSalary(empNo,request);
-        return WebResponse.<SalaryResponse>builder()
+        WebResponse<SalaryResponse> webResponse = WebResponse.<SalaryResponse>builder()
                 .data(salaryResponse)
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).updateSalary(empNo, request)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @DeleteMapping(
@@ -78,9 +93,12 @@ public class SalaryController {
     )
     public WebResponse<String> deleteSalary(@PathVariable("empNo")Integer empNo){
         salaryService.deleteSalary(empNo);
-        return WebResponse.<String>builder()
+        WebResponse<String> webResponse = WebResponse.<String>builder()
                 .data("OK")
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).deleteSalary(empNo)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @GetMapping(
@@ -91,7 +109,7 @@ public class SalaryController {
                                                           @RequestParam(value = "page",required = false,defaultValue = "0")Integer page,
                                                           @RequestParam(value = "size",required = false,defaultValue = "10") Integer size){
         Page<SalaryResponse> salaryResponses = salaryService.searchSalary(keyword,page,size);
-        return WebResponse.<List<SalaryResponse>>builder()
+        WebResponse<List<SalaryResponse>> webResponse = WebResponse.<List<SalaryResponse>>builder()
                 .data(salaryResponses.getContent())
                 .paging(PagingResponse.builder()
                         .currentPage(salaryResponses.getNumber())
@@ -99,6 +117,10 @@ public class SalaryController {
                         .size((int) salaryResponses.getTotalElements())
                         .build())
                 .build();
+        Link selfLink = linkTo(methodOn(SalaryController.class).searchSalary(keyword,page,size)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
+
 
     }
 }
--- src/main/java/com/bagas/springrestapi/controller/TitleController.java
+++ src/main/java/com/bagas/springrestapi/controller/TitleController.java
@@ -4,11 +4,15 @@ import com.bagas.springrestapi.model.*;
 import com.bagas.springrestapi.service.TitleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -24,9 +28,12 @@ public class TitleController {
     )
     public WebResponse<String> regiterTitle(@RequestBody RegisterTitleRequest request) throws ParseException {
         titleService.registerTitle(request);
-        return WebResponse.<String>builder()
+        WebResponse<String> webResponse = WebResponse.<String>builder()
                 .data("OK")
                 .build();
+        Link selfLink = linkTo(methodOn(TitleController.class).regiterTitle(request)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @PutMapping(
@@ -36,9 +43,12 @@ public class TitleController {
     public WebResponse<TitleResponse> updateTitle(@PathVariable("empNo")Integer empNo,
                                                   @RequestBody UpdateTitleRequest request) throws ParseException {
         TitleResponse titleResponse = titleService.updateTitle(empNo,request);
-        return WebResponse.<TitleResponse>builder()
+        WebResponse<TitleResponse> webResponse = WebResponse.<TitleResponse>builder()
                 .data(titleResponse)
                 .build();
+        Link selfLink = linkTo(methodOn(TitleController.class).updateTitle(empNo,request)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @GetMapping(
@@ -48,9 +58,12 @@ public class TitleController {
     public WebResponse<TitleResponse> getTitleByEmpNo(@PathVariable("empNo")Integer empNo){
 
         TitleResponse titleResponse = titleService.getTitleByEmpNo(empNo);
-        return WebResponse.<TitleResponse>builder()
+        WebResponse<TitleResponse> webResponse = WebResponse.<TitleResponse>builder()
                 .data(titleResponse)
                 .build();
+        Link selfLink = linkTo(methodOn(TitleController.class).getTitleByEmpNo(empNo)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @DeleteMapping(
@@ -59,9 +72,12 @@ public class TitleController {
     )
     public WebResponse<String> deleteTitle(@PathVariable("empNo") Integer empNo){
         titleService.deleteTitle(empNo);
-        return WebResponse.<String>builder()
+        WebResponse<String> webResponse = WebResponse.<String>builder()
                 .data("OK")
                 .build();
+        Link selfLink = linkTo(methodOn(TitleController.class).deleteTitle(empNo)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @GetMapping(
@@ -72,14 +88,16 @@ public class TitleController {
                                                         @RequestParam(value = "size",required = false,defaultValue = "10")Integer size){
 
         Page<TitleResponse> titleResponses = titleService.getAllTitle(page, size);
-        return WebResponse.<List<TitleResponse>>builder()
+        WebResponse<List<TitleResponse>> webResponse = WebResponse.<List<TitleResponse>>builder()
                 .data(titleResponses.getContent())
                 .paging(PagingResponse.builder()
                         .currentPage(titleResponses.getNumber())
                         .totalPage(titleResponses.getTotalPages())
                         .size((int) titleResponses.getTotalElements()).build())
                 .build();
-
+        Link selfLink = linkTo(methodOn(TitleController.class).getAllTitle(page, size)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
     }
 
     @GetMapping(
@@ -91,13 +109,16 @@ public class TitleController {
                                                         @RequestParam(value = "size",required = false,defaultValue = "10")Integer size){
 
         Page<TitleResponse> titleResponses = titleService.searchTitle(keyword,page, size);
-        return WebResponse.<List<TitleResponse>>builder()
+        WebResponse<List<TitleResponse>> webResponse = WebResponse.<List<TitleResponse>>builder()
                 .data(titleResponses.getContent())
                 .paging(PagingResponse.builder()
                         .currentPage(titleResponses.getNumber())
                         .totalPage(titleResponses.getTotalPages())
                         .size((int) titleResponses.getTotalElements()).build())
                 .build();
+        Link selfLink = linkTo(methodOn(TitleController.class).searchTitle(keyword,page, size)).withSelfRel();
+        webResponse.add(selfLink);
+        return webResponse;
 
     }
 
